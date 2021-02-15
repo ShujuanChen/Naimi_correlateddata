bd7de0cab798112f5a4607c1e38e71f878707ccb
Ashley Naimi <ashley.naimi@emory.edu>
2021-02-14 22:45:34 -0500

    fully updated simulation

--- R/cluster_sim.R
+++ R/cluster_sim.R
@@ -1,3 +1,5 @@
+userLib <- "~/R/R_LIBS_USER"
+.libPaths(userLib)
 packages <- c("data.table","tidyverse","skimr","here","lmtest","sandwich",
               "geepack","lme4","RColorBrewer","numDeriv","wgeesel","MASS")
 
@@ -11,32 +13,26 @@ for (package in packages) {
   library(package, character.only=T)
 }
 
-remotes::install_github("ikosmidis/enrichwith")
-library(enrichwith)
-
-thm <- theme_classic() +
-  theme(
-    legend.position = "top",
-    legend.background = element_rect(fill = "transparent", colour = NA),
-    legend.key = element_rect(fill = "transparent", colour = NA)
-  )
-theme_set(thm)
+# install.packages("simstudy")
+# library(simstudy)
 
 ## simulated BMI example to demonstrate MLE with clustered data
 set.seed(123)
-n=100000
-id=rep(1:n,each=6)
-rho=0
+
+sim_res <- function(rho){
+n=400
+id=rep(1:n,each=5)
+#rho=.75
 phi=1
-s=3.25
-exposure <- rep(1,length(id)) #rbinom(length(id),1,.5)
+#s=runif(n,1.25,5)
+s=rep(3.25,n)
+exposure <- rbinom(length(id),1,.5) #rep(1,length(id)) #
 x=cbind(1,exposure)
 beta=c(22,3.25)
 
-cont_fun <- function(beta,rho,phi,sigma,x,id,corstr="independence"){ #N is the sample size, nsubj is cluster size;
+cont_fun <- function(beta,rho,phi,sigma,x,id,corstr){ #N is the sample size, nsubj is cluster size;
   response=c()
   N=length(unique(id))
-  i=1
   for(i in 1:N){
     mu_i <- x[which(id==i),]%*%beta 
     if(corstr=="independence"){
@@ -44,13 +40,14 @@ cont_fun <- function(beta,rho,phi,sigma,x,id,corstr="independence"){ #N is the s
     }
     else if (corstr=="exchangeable"){
       R_i=diag(1,length(mu_i))+matrix(rho,ncol=length(mu_i),nrow=length(mu_i))-diag(rho,length(mu_i))
+
     }
     else {
       H <- abs(outer(1:length(mu_i),1:length(mu_i), "-")) 
       R_i=rho^H
     }
     
-    S_i=rep(sigma,length(mu_i))*diag(length(mu_i))
+    S_i=rep(sigma[i],length(mu_i))*diag(length(mu_i))
     m=phi*(sqrt(S_i)%*%R_i%*%sqrt(S_i))
     response <- c(response,mvrnorm(n = 1, mu=mu_i, Sigma=m))
   }
@@ -60,15 +57,56 @@ cont_fun <- function(beta,rho,phi,sigma,x,id,corstr="independence"){ #N is the s
   return(data)
 }
 
-sim_dat <- cont_fun(beta,rho,phi,s,x,id,corstr="independence")
+sim_dat <- cont_fun(beta,rho,phi,s,x,id,corstr="exchangeable")
 
 names(sim_dat)[2] <- "exposure"
-
-head(sim_dat)
-mean(sim_dat$exposure)
-mean(sim_dat$response)
-var(sim_dat$response)
+sim_dat <- sim_dat %>% 
+  group_by(id) %>% 
+  mutate(count=1,time=cumsum(count))
+# 
+# ggplot(sim_dat) + geom_point(aes(x=id,y=response))
+# 
+# ggplot(sim_dat) + 
+#   geom_line(aes(x=time,
+#                 y=response,
+#                 group=as.factor(id),
+#                 color=as.factor(exposure)),
+#           size=.5,alpha=.5) +
+#   geom_point(aes(x=time,
+#                  y=response,
+#                  group=as.factor(id), 
+#                  color=as.factor(exposure)),
+#              size=1.5,alpha=.5) +
+#   xlab("Time Since T0") +
+#   ylab("Outcome") +
+#   labs(color="Exposure") +
+#   scale_colour_grey(start = 0, end = .8) +
+#   scale_y_continuous(expand=c(0,0)) +
+#   scale_x_continuous(expand=c(.01,0))
+# 
+# head(sim_dat,12)
+# mean(sim_dat$exposure)
+# mean(sim_dat$response)
+# var(sim_dat$response)
 
 sim_summary <- summary(aov(response ~ as.factor(id),data=sim_dat)) # type II SS
 icc <- sim_summary[[1]][1,2]/sum(sim_summary[[1]][,2])
 icc
+
+mod1 <- glm(response ~ exposure,data=sim_dat,family=gaussian(link="identity"))
+
+res <- c(rho,icc,summary(mod1)$coefficients[2,1:2])
+
+return(res)
+
+}
+
+vec <- rep(c(0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1),10)
+res <- lapply(vec, function(x) sim_res(rho=x))
+res <- do.call(rbind,res)
+
+write.csv(res,"./data/sim_res.csv")
+
+
+
+
